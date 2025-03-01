0ccb233c2d69aeb5d9e558cf6f209b3f288140fa
stuartash <64287842+stuart-fruitStreet@users.noreply.github.com>
2025-02-28 18:36:37 -0800

    fixerd addTodo
    fix

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,38 +1,37 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
 
 type AddTodoProps = {
   addTodoItem: (todo: string) => void;
-}
+};
 
 export default function AddTodo({ addTodoItem }: AddTodoProps) {
-  const [todo, setTodo] = useState<string>('');
+  const [todo, setTodo] = useState<string>("");
   const handleAddTodo = (todo: string) => {
     todo = todo.trim();
-    if(!todo) return;
+    if (!todo) return;
     addTodoItem(todo);
-    setTodo('');
-  }
+    setTodo("");
+  };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleAddTodo(todo);
     }
-  }
+  };
 
   return (
-    <div className='add-todo'>
+    <div className="add-todo">
       <h2>Add Todo Item:</h2>
       <div className="add-todo">
-        <input 
-          type="text" 
-          value={todo} 
-          onChange={(e) => setTodo(e.target.value)} 
-          onKeyDown={handleKeyPress} 
-          placeholder="Enter todo item" 
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          onKeyDown={handleKeyPress}
+          placeholder="Enter todo item"
         />
         <button onClick={() => handleAddTodo(todo)}>Add</button>
       </div>
     </div>
-  )
+  );
 }
